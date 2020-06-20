13d029e70d092907e5f895a66dfb942e17622f65
DariuszLegizynski <d.legizynski@gmail.com>
2020-06-20 06:45:03 +0200

    Getting results from API Server. Axios still has a bug and is not merging its parameters

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 import NavBar from "./NavBar";
+import youtube from "../apis/youtube";
 
 function App() {
-	const [ searchedTerm, setSearchedTerm ] = useState("");
+	const [ searchedValue, setSearchedValue ] = useState({ videos: [] });
 
-	const handleSearchFromApp = (inputText) => {
+	const API_KEY = process.env.REACT_APP_API_KEY;
+
+	const handleSearch = async (inputText) => {
+		const response = await youtube.get("/search", {
+			params: {
+				q: inputText,
+				part: "snippet",
+				type: "video",
+				maxResults: 5,
+				key: API_KEY
+			}
+		});
 		console.log(inputText);
-		setSearchedTerm(inputText);
+		console.log(response);
+		setSearchedValue({ videos: response.data.items });
 	};
 
 	return (
 		<div className="ui container">
-			<NavBar handleSearch={handleSearchFromApp} />
-			<p>Search result for (my text written in the searchBar): {searchedTerm}</p>
+			<NavBar handleSearch={handleSearch} />
+			<p>for "the typed in string" I got {searchedValue.videos.length} results.</p>
 		</div>
 	);
 }
