d0950f024e30ab22507f2611e86df977a0c2561e
DariuszLegizynski <d.legizynski@gmail.com>
2020-07-20 15:51:05 +0200

    added the functionality to add new lists. However changed the styles from <a href> to <div> so need to fix it.

---
+++ src/components/CreateNewList.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import iconSprites from "../images/sprite.svg";
+
+const CreateNewList = (props) => {
+	const [ list, setList ] = useState({
+		title: ""
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setList((prevList) => {
+			return {
+				...prevList,
+				[name]: value
+			};
+		});
+	};
+
+	const submitNewList = (event) => {
+		props.onAdd(list);
+		event.preventDefault();
+	};
+
+	return (
+		<li className="side-nav__item">
+			<form>
+				<div className="side-nav__link">
+					<button onClick={submitNewList}>
+						<svg className="side-nav__icon">
+							<use href={iconSprites + "#icon-circle-with-plus"} />
+						</svg>
+					</button>
+					<input
+						name="title"
+						value={list.title}
+						onChange={handleChange}
+						placeholder="New List"
+					/>
+				</div>
+			</form>
+		</li>
+	);
+};
+
+export default CreateNewList;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBarList from "./SideBarList";
+import CreateNewList from "./CreateNewList";
 
 import iconSprites from "../images/sprite.svg";
 import "./SideBar.css";
 
 const SideBar = () => {
+	const [ lists, setLists ] = useState([]);
+
+	const addList = (newList) => {
+		setLists((prevLists) => {
+			return [ ...prevLists, newList ];
+		});
+	};
+
 	return (
 		<nav className="sidebar">
 			<ul className="side-nav">
 				{SideBarList.map((list) => {
 					return (
 						<li className="side-nav__item">
-							<a href="Link1" className="side-nav__link">
+							<div className="side-nav__link">
 								<svg className="side-nav__icon">
 									<use href={iconSprites + "#icon-list"} />
 								</svg>
@@ -19,19 +28,28 @@ const SideBar = () => {
 								<svg className="side-nav__icon-minus">
 									<use href={iconSprites + "#icon-circle-with-minus"} />
 								</svg>
-							</a>
+							</div>
 						</li>
 					);
 				})}
-
-				<li className="side-nav__item">
-					<a href="Link3" className="side-nav__link">
-						<svg className="side-nav__icon">
-							<use href={iconSprites + "#icon-circle-with-plus"} />
-						</svg>
-						<span>New List</span>
-					</a>
-				</li>
+				{lists.map((newList) => {
+					return (
+						<li className="side-nav__item">
+							<div className="side-nav__link">
+								<svg className="side-nav__icon">
+									<use href={iconSprites + "#icon-list"} />
+								</svg>
+								<span>{newList.title}</span>
+								<svg className="side-nav__icon-minus">
+									<use href={iconSprites + "#icon-circle-with-minus"} />
+								</svg>
+							</div>
+						</li>
+					);
+				})}
+				<React.Fragment>
+					<CreateNewList onAdd={addList} />
+				</React.Fragment>
 			</ul>
 
 			<div className="legal">&copy; 2020 by Readeo. All rights reserved.</div>
