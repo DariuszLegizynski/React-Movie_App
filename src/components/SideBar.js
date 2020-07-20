7a6940af5aff1e7534d4453a0958cb61abf44b06
DariuszLegizynski <d.legizynski@gmail.com>
2020-07-20 13:40:08 +0200

    added ready list positions from a small list database

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SideBarList from "./SideBarList";
 
 import iconSprites from "../images/sprite.svg";
 import "./SideBar.css";
@@ -7,22 +8,19 @@ const SideBar = () => {
 	return (
 		<nav className="sidebar">
 			<ul className="side-nav">
-				<li className="side-nav__item">
-					<a href="Link1" className="side-nav__link">
-						<svg className="side-nav__icon">
-							<use href={iconSprites + "#icon-list"} />
-						</svg>
-						<span>Animations</span>
-					</a>
-				</li>
-				<li className="side-nav__item">
-					<a href="Link2" className="side-nav__link">
-						<svg className="side-nav__icon">
-							<use href={iconSprites + "#icon-list"} />
-						</svg>
-						<span>Music</span>
-					</a>
-				</li>
+				{SideBarList.map((list) => {
+					return (
+						<li className="side-nav__item">
+							<a href="Link1" className="side-nav__link">
+								<svg className="side-nav__icon">
+									<use href={iconSprites + "#icon-list"} />
+								</svg>
+								<h4>{list.title}</h4>;
+							</a>
+						</li>
+					);
+				})}
+
 				<li className="side-nav__item">
 					<a href="Link3" className="side-nav__link">
 						<svg className="side-nav__icon">
---
+++ src/components/SideBarList.js
@@ -0,0 +1,12 @@
+const SideBarList = [
+	{
+		key: 1,
+		title: "Science"
+	},
+	{
+		key: 2,
+		title: "cats and dogs"
+	}
+];
+
+export default SideBarList;
