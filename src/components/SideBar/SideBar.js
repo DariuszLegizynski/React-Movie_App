018f4aba8e2fb6e9fd1514a47f8de5542f33dbe8
DariuszLegizynski <d.legizynski@gmail.com>
2020-09-21 08:43:47 +0200

    accidently added memo into Sidebar. Now removed.

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React from "react";
 import CreateNewList from "../CreateNewList/CreateNewList";
 import NewList from "../NewList/NewList";
 import Footer from "../Footer/Footer";
@@ -7,44 +7,42 @@ import shortid from "shortid";
 
 import "./SideBar.css";
 
-const SideBar = memo(
-	({
-		openSidebar,
-		handleSelectedFavorite,
-		favoritedList,
-		onDeleteFavorited,
-		addList,
-		lists,
-		onDeleteList
-	}) => {
-		let sidebarClasses = "sidebar";
-		if (openSidebar) {
-			sidebarClasses = "sidebar open";
-		}
-
-		return (
-			<nav className={sidebarClasses}>
-				<ul className="side-nav">
-					{/* //This part keeps track of the newly created lists and also allows to delete them */}
-					{lists.map((newList, index) => {
-						return (
-							<NewList
-								key={shortid.generate()}
-								title={newList.title}
-								index={index}
-								onDeleteList={onDeleteList}
-								handleSelectedFavorite={handleSelectedFavorite}
-								favoritedList={favoritedList}
-								onDeleteFavorited={onDeleteFavorited}
-							/>
-						);
-					})}
-					<CreateNewList onAdd={addList} />
-				</ul>
-				<Footer />
-			</nav>
-		);
+const SideBar = ({
+	openSidebar,
+	handleSelectedFavorite,
+	favoritedList,
+	onDeleteFavorited,
+	addList,
+	lists,
+	onDeleteList
+}) => {
+	let sidebarClasses = "sidebar";
+	if (openSidebar) {
+		sidebarClasses = "sidebar open";
 	}
-);
+
+	return (
+		<nav className={sidebarClasses}>
+			<ul className="side-nav">
+				{/* //This part keeps track of the newly created lists and also allows to delete them */}
+				{lists.map((newList, index) => {
+					return (
+						<NewList
+							key={shortid.generate()}
+							title={newList.title}
+							index={index}
+							onDeleteList={onDeleteList}
+							handleSelectedFavorite={handleSelectedFavorite}
+							favoritedList={favoritedList}
+							onDeleteFavorited={onDeleteFavorited}
+						/>
+					);
+				})}
+				<CreateNewList onAdd={addList} />
+			</ul>
+			<Footer />
+		</nav>
+	);
+};
 
 export default SideBar;
